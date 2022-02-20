4d0cace39371da6b156a6374153bfb0c8596b30d
Glowman554 <glowman434@gmail.com>
2022-02-20 10:03:20 +0100

    added network syscalls

--- test/test.c
+++ test/test.c
@@ -6,9 +6,20 @@
 
 int main(int argc, char* argv[], char* envp[]) {
 
-	union ip_u ip = dns_resolve("discord.com", 0);
+	union ip_u ip = dns_resolve("www.gsz-zak.de", 0);
+	printf("%d.%d.%d.%d\n", ip.ip_p[0], ip.ip_p[1], ip.ip_p[2], ip.ip_p[3]);
+	char* http_request = "GET /robots.txt HTTP/1.1\r\nHost: www.gsz-zak.de\r\n\r\n";
 
-	printf("ip: %d.%d.%d.%d\n", ip.ip_p[0], ip.ip_p[1], ip.ip_p[2], ip.ip_p[3]);
-	printf("%s\n", icmp_echo_request(ip.ip, 0) ? "got response" : "no response");
+	int socket = connect(ip.ip, 80, TCP_SOCKET);
+	send(socket, http_request, strlen(http_request));
+
+	char* buffer = malloc(1024);
+	int bytes_read = recv(socket, buffer, 1024);
+	buffer[bytes_read] = 0;
+	printf("%s\n", buffer);
+	free(buffer);
+
+	disconnect(socket);
+
     return 0;
 }
