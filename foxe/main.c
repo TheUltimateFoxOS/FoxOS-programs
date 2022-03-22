3893d22f2cd333c774003431322e91b5105c296b
MoritzSchittenhelm <78039560+JartC0ding@users.noreply.github.com>
2022-03-22 18:23:22 +0100

    Foxe updates

--- foxe/main.c
+++ foxe/main.c
@@ -4,25 +4,6 @@
 #include <string.h>
 #include <sys/env.h>
 #include <sys/write.h>
-
-#include <stddef.h>
-void* memmove(void *dest, const void *src, size_t len)
-{
-  char *d = dest;
-  const char *s = src;
-  if (d < s)
-    while (len--)
-      *d++ = *s++;
-  else
-    {
-      char *lasts = s + (len-1);
-      char *lastd = d + (len-1);
-      while (len--)
-        *lastd-- = *lasts--;
-    }
-  return dest;
-}
-
 #define CHAR_SIZE 16
 
 char* input_buffer;
