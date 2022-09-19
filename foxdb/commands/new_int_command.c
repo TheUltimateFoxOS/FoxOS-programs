a7a1d3ddae04ea6c56cdc642910ba2108b6df55e
Glowman554 <glowman434@gmail.com>
2022-09-19 09:04:01 +0200

    use __libc_parse_number

--- foxdb/commands/new_int_command.c
+++ foxdb/commands/new_int_command.c
@@ -1,10 +1,13 @@
 #include <db_commands.h>
 #include <foxdb.h>
+#include <buildin/number_parser.h>
 
 void* new_int(int argc, char** argv, void* db) {
 	x_args(2);
 
-	int i = (int) strtol(argv[2], NULL, 10);
+	int i;
+	__libc_parse_number(argv[2], &i);
+	
 	foxdb_int_t* e = foxdb_int(argv[1], i);
 
 	db = foxdb_insert(db, (foxdb_entry_t*) e);
